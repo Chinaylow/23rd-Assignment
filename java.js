f8be8d4ebdfddd92666acd8c285bd40714b5c733
chinaylow <dinyelucynthia@gmail.com>
2022-05-05 15:50:00 +0100

    first commit

---
+++ java.js
@@ -0,0 +1,26 @@
+var array1 = [20,46,90,80,120];
+var array2 = [2,2,5,4,2];
+var output1 = array1[0]/array2[0];
+var output2 = array1[1]/array2[1];
+var output3 = array1[2]/array2[2];
+var output4 = array1[3]/array2[3];
+var output5 = array1[4]/array2[4];
+var output = `${output1},${output2},${output3},${output4} and ${output5}`;
+console.log(output);
+
+// ? Question 2:Multi dimensional Arrays
+var arr = [[1, 2, 3],[4, 5, 6],7,8,9,[[10, 11, 12, [13, 4, 112]], 136, 14],];
+console.log(arr[5][0][0],arr[3],arr[5][0][3][0],arr[5][1],arr[1][0]);
+// for (var i = 0; i < arr.length; i++){
+//     console.log(arr[i][]);
+    // for(var j = 0; j < arr[i].length; j++ ){
+    //     console.log(arr[i][j])
+    // }
+
+        // for(var k = 0; k< arr[i][j].length; k++){   
+        //     console.log(arr[i][j]);
+
+        // }
+        // var arr3 = arr2[j]
+// }
+//I have tried all i could, researched anbd asked questions too
